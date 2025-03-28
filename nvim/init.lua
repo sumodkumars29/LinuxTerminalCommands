a3e7cb8eaa078c2afaed4c4aa850227a5f7efce6
sumodk <sumodkumars29@gmail.com>
2025-03-28 13:47:46 +0530

    updated with the latest neowim config that works

---
+++ nvim/init.lua
@@ -0,0 +1,106 @@
+require 'core.options' -- Load general options (options.lua from lua/core/)
+require 'core.keymaps' -- Load keymaps (keymaps.lua from lua/core/)
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+
+
+
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- NOTE: Plugins can also be added by using a table, with the first argument being the link and the following
+  -- keys can be used to configure plugin behavior/loading/etc.
+  -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
+  -- Alternatively, use `config = function() ... end` for full control over the configuration.
+  -- If you prefer to call `setup` explicitly, use:
+  --    {
+  --        'lewis6991/gitsigns.nvim',
+  --        config = function()
+  --            require('gitsigns').setup({
+  --                -- Your  configuration here
+  --            })
+  --        end,
+  --    }
+--  {
+--    "nvim-neo-tree/neo-tree.nvim",
+--    branch = "v3.x",
+--    dependencies = {
+--      "nvim-lua/plenary.nvim",
+--      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+--      "MunifTanjim/nui.nvim",
+--      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+--    }
+--  },
+    {
+      "christoomey/vim-tmux-navigator"
+    },
+
+    require 'plugins.colortheme',
+    require 'plugins.neotree',
+    require 'plugins.bufferline',
+    require 'plugins.lualine',
+    require 'plugins.which_key',
+    require 'plugins.telescope',
+
+{
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+
+  config = function()
+    -- import nvim-treesitter plugin
+    local treesitter = require("nvim-treesitter.configs")
+
+    -- configure treesitter
+    treesitter.setup({
+      highlight = { enable = true }, -- enable syntax highlighting
+      indent = { enable = true },    -- enable indentation
+      ensure_installed = {
+        "json", "javascript", "typescript", "tsx", "yaml",
+        "html", "css", "prisma", "markdown", "markdown_inline",
+        "svelte", "graphql", "bash", "lua", "vim", "dockerfile",
+        "gitignore", "query", "vimdoc", "c", "cpp", "php",
+        "php_only", "python", "sql",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+
+    -- setup nvim-ts-autotag separately
+    require("nvim-ts-autotag").setup({})
+  end,
+}
+
+
+  })
+
+
+
+
+
+
---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,62 @@
+-- set leader key to space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local keymap = vim.keymap -- for conciseness
+local opts = { noremap = true, silent = true }
+
+---------------------
+-- General Keymaps -------------------
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Buffers
+--vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
+--vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+--vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts) -- close buffer
+--vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
+
+
+vim.g.tmux_navigator_no_mappings = 1  -- Disable default mappings
+
+-- Manually set split navigation inside Neovim
+vim.keymap.set("n", "<BS>", "<C-w>h", opts, { desc = "Move left (C-h and Backspace)" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts, { desc = "Move down (Ctrl+j)" }) 
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts, { desc = "Move up (Ctrl+k)" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts, { desc = "Move right (Ctrl+l)" })
+
+
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts, { desc = "Toggle line wrapping (<Space>+lw)" })
+
+-- Stay in indent mode
+vim.keymap.set('v', '<', '<gv', opts, { desc = "Indent selection to the left and remain in VISUAL mode" })
+vim.keymap.set('v', '>', '>gv', opts, { desc = "Indent selection to the right and remain in VISUAL mode" })
+
+-- Keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts, { desc = "After yanking with 'y' and pasting over a selection, the copied text is retained in the clipboard and not over written with the text that was paster over." })
+
+
